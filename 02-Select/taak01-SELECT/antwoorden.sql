75cb39654886c1c4399e5a45c7c6e4ff91cf6ce8
Jorrit van Geffen <jorritvgeffen@gmail.com>
2020-11-18 16:05:05 +0100

    :(

--- 02-Select/taak01-SELECT/antwoorden.sql
+++ 02-Select/taak01-SELECT/antwoorden.sql
@@ -1,13 +1,14 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * FROM jaar2015
 -- Opdracht 3
-
+SELECT * FROM jaar2016`country`
 -- Opdracht 4
-
+SELECT * FROM jaar2015`region`
 -- Opdracht 5
-
+SELECT * FROM jaar2015`region``score`
 -- Opdracht 6
-
+SELECT * FROM jaar2016`score``rank`
 -- Opdracht 7
+-- Deze opdracht bestaat niet 
--- 02-Select/taak03-AND-OR/antwoorden.sql
+++ 02-Select/taak03-AND-OR/antwoorden.sql
@@ -1,15 +1,41 @@
 -- Opdracht 1 
-
+Bregas, Costa, Azpilicueta, Pedro, Marcos Alonso, Morata
 -- Opdracht 2 
-
+Brahim D az
 -- Opdracht 3
-
+Wilson & Gomez
 -- Opdracht 4
-
+Huntelaar
+De jong
+viergever
+van leer
+veltman
+dijks
+sinkgraven
+nouri
+van de beek
+alblas
+de jong
+dankerlui
+eiting
+de ligt
+zeefuik
+kluivert
 -- Opdracht 5
-
+Schone
+Lamprou
+Younes
+Ziyech
+?ern
+Orejuela
+Onana
+Cassierra
+Dolberg
+W Ber
+Neres
+Johnsen
 -- Opdracht 6 
-
+.........
 -- Opdracht 7 
 
 -- Opdracht 8
