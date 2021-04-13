8edac04cc7d88f2820578d88b96d80f94b48e438
Jorrit van Geffen <jorritvgeffen@gmail.com>
2021-04-13 14:12:24 +0200

    kom niet verde

--- opdracht/antwoorden.sql
+++ opdracht/antwoorden.sql
@@ -6,26 +6,28 @@ SELECT * FROM `videogamesales` WHERE year="1999"
 SELECT SUM(NA_Sales) FROM videogamesales; 
 --Antwoord is 191002
 -- Opdracht 4
-SELECT `name`, `platform` FROM `videogamesales`, WHERE publisher = "Nintendo";
+SELECT `name`, `platform` , `publisher` , `year`
+FROM `videogamesales` 
+WHERE publisher = "Nintendo"
+AND year > 1990 AND year < 2005
 -- Opdracht 5
-SELECT * FROM `videogamesales` WHERE `Global_Sales` > 5000
--- Opdracht 6 
-SELECT * FROM `videogamesales` WHERE `genre` = 'Puzzle'
+USE videogamesales;
+SELECT name, MAX(Global_Sales) AS 'Max Value'
+FROM videogamesales
+--Antwoord is Wii met 8274 sales
+-- Opdracht 6
+
 -- Opdracht 7 
-SELECT `name` FROM `videogamesales` WHERE `JP_Sales` = 532
+
 -- Opdracht 8
-SELECT * FROM `videogamesales` WHERE `publisher` = 'Nintendo'
+
 -- Opdracht 9
-SELECT * FROM `videogamesales` WHERE `genre`="Racing" AND `publisher`="Nintendo" OR "Activision"
+
 -- Opdracht 10
 -
 -- Opdracht 11
-DELETE FROM `videogamesales` WHERE `name`="Halo 2"
 -- Opdracht 12
-DELETE FROM `videogamesales` WHERE `year`="2012" AND `publisher`="Ubisoft"
 -- Opdracht 13
-DELETE FROM `videogamesales` WHERE `genre`="Adventure" AND `publisher`="Nintendo"
 -- Opdracht 14
-DELETE FROM `videogamesales` WHERE `publisher`="Nintendo" AND `Global_Sales`="<1000"
 -- Opdracht 15
-DELETE FROM `videogamesales` WHERE `year`="1997" AND `NA_Sales`="<200000"
+
